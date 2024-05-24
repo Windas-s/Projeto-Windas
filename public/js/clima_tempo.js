3d57d5eb9e1fe2e4d8a5e34b98dde7c9d6bf94f3
PajorVi <vinicius.marques@sptech.school>
2024-05-23 22:12:07 -0300

    inicio da lógica da API

---
+++ public/js/clima_tempo.js
@@ -0,0 +1,5 @@
+const apiKey = '910c4efbe73c746cce373fd23d6fdf43';
+const cidade = 'São Paulo'
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cidade)}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+async(chamarApi) => {}
